69235a82f16c4765dc86ede39a92a7ed9120f4c2
Ricardo Olarte <raop155@hotmail.com>
2021-06-24 15:57:11 -0500

    Toggle Button & UI Service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,15 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Task Tracker';
+  title:string = 'Task Tracker';
+  showAddTask:boolean = false;
+  subscription!:Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    // console.log("toggle");
+    this.uiService.toggleAddTask();
   }
 
 }
